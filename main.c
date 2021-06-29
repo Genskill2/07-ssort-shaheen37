c99fa55a595eaa7e8c3bd3cbe724459b7067b1c0
shaheen37 <85019052+shaheen37@users.noreply.github.com>
2021-06-29 19:38:46 +0530

    Update main.c

--- main.c
+++ main.c
@@ -65,3 +65,12 @@ void swap_max(int array[], int length, int n)
     array[largest]= array[n];
     array[n]=temp;
 }
+
+void ssort(int array[], int length)
+{
+  for(int i=0;i<length-1;i++)
+  {
+    swap_max(array,length, i);
+  }
+}
+
